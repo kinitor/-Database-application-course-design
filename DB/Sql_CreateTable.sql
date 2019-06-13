d681270016ef1c64373c091c9985d02e1ac10a02
YLH <42891308+Q1311605467@users.noreply.github.com>
2019-06-13 18:11:34 +0800

    Add files via upload

---
+++ DB/Sql_CreateTable.sql
@@ -0,0 +1,84 @@
+
+--Bookstore retail management system 书店零售管理系统
+create database BRMSDB  --建立数据库
+on
+(
+name='BRMSDB',--创建数据文件名
+filename='d:\db\BRMSDB.mdf',--文件名地址
+size=3mb,--文件起始大小
+maxsize=100mb,--文件最大大小
+filegrowth=2mb--文件增长速度
+)
+log on
+(
+name='StuLog',
+filename='d:\db\BRMSDB.ldf',
+size=1mb,
+maxsize=10mb,
+filegrowth=10%
+)
+------------------------------------------------------------------------------------
+
+use BRMSDB
+go
+drop table supplier
+go
+create table Supplier--供应商表
+(
+	Supplier_id varchar(20) primary key,--供应商号
+	Supplier_name varchar(20),--供应商名字
+	Supplier_city varchar(20),--供应商城市
+	Supplier_tel varchar(20),--供应商电话
+	
+)
+
+create table Orderform--订单表
+(
+	Orderform_id varchar(20) primary key,--订单编号
+	Orderform_time datetime,--订单时间
+	Customer_id varchar(20),--顾客编号
+	Supplier_id varchar(20),--供应商编号
+	Supplier_total int--供应量
+)
+
+create table Order_detail--订单明细表
+(
+	Orderform_id varchar(20) primary key,--订单编号
+	Book_num int ,--图书数量
+	Book_id varchar(20),--图书号
+	Pay_total float--小计
+)
+--drop table Book
+
+create table Book--图书表
+(
+	Book_id varchar(20) primary key,--图书号
+	Book_in_price float,--进价
+	Book_out_price float,--图书售价
+	Book_name varchar(20),--书名
+	Supplier_name varchar(20),--供应商名字
+	Book_stock int--库存
+)
+create table Pay_message--支付信息表
+(
+	Orderform_id varchar(20) primary key,--订单号
+	Pay_money float,--支付总额
+	Pay_mode varchar(20)--支付方式
+)
+create table Customer--顾客表
+(
+	Customer_id varchar(20) primary key,--顾客号
+	Customer_vip bit,--是否会员
+	Customer_vip_money float,--会员卡金额
+	Customer_tel varchar(20) --顾客联系电话
+) 
+--drop table Sell_record
+create table Sell_record--销售表
+(
+	Orderform_id varchar(20),--订单号
+	Book_id varchar(20),--图书号
+	PRIMARY KEY(Orderform_id,Book_id)
+)
+------------------------------------------------------------------------------------------------
+
+
---
+++ DB/Sql_insert.sql
@@ -0,0 +1,94 @@
+create procedure add_customer_message--添加用户信息存储过程
+    @Customer_id varchar(20) ,
+	@Customer_vip bit,--是否会员
+	@Customer_vip_money float,--会员卡金额
+	@Customer_tel varchar(20) --顾客联系电话
+as
+insert into Customer(Customer_id,Customer_vip,Customer_vip_money,Customer_tel)
+values (@Customer_id,@Customer_vip,@Customer_vip_money,@Customer_tel)
+
+
+DROP TABLE BOOK
+
+exec add_book_message 'BK10000', 31.00, 44.00, '概率论', '科学出版社', 5 
+exec add_book_message 'BK10001', 37.00, 50.00, '算法导论', '北京出版社', 5 
+exec add_book_message 'BK10002', 24.00, 37.00, '数据库开发与应用', '人民邮电出版社', 5 
+exec add_book_message 'BK10003', 30.00, 43.00, 'java程序设计教程', '高等教育出版社', 5 
+exec add_book_message 'BK10004', 49.00, 62.00, '数据库系统概论', '清华大学出版社', 5 
+exec add_book_message 'BK10005', 24.00, 37.00, '算法与数据结构', '中国人民大学出版社', 5 
+exec add_book_message 'BK10006', 38.00, 51.00, '离散数学', '科学出版社', 5 
+exec add_book_message 'BK10007', 38.00, 51.00, '概率论', '北京出版社', 5 
+exec add_book_message 'BK10008', 42.00, 55.00, '算法导论', '人民邮电出版社', 5 
+exec add_book_message 'BK10009', 34.00, 47.00, '数据库开发与应用', '高等教育出版社', 5 
+exec add_book_message 'BK10010', 25.00, 38.00, 'java程序设计教程', '清华大学出版社', 5 
+exec add_book_message 'BK10011', 25.00, 38.00, '数据库系统概论', '中国人民大学出版社', 5 
+exec add_book_message 'BK10012', 21.00, 34.00, '算法与数据结构', '科学出版社', 5 
+exec add_book_message 'BK10013', 47.00, 60.00, '离散数学', '北京出版社', 5 
+exec add_book_message 'BK10014', 21.00, 34.00, '概率论', '人民邮电出版社', 5 
+exec add_book_message 'BK10015', 31.00, 44.00, '算法导论', '高等教育出版社', 5 
+ 
+
+--------------------------------------------------------------------------------
+
+create procedure add_customer_message--添加用户信息存储过程
+    @Customer_id varchar(20) ,
+	@Customer_vip bit,--是否会员
+	@Customer_vip_money float,--会员卡金额
+	@Customer_tel varchar(20) --顾客联系电话
+as
+insert into Customer(Customer_id,Customer_vip,Customer_vip_money,Customer_tel)
+values (@Customer_id,@Customer_vip,@Customer_vip_money,@Customer_tel)
+
+create procedure add_book_message
+	@Book_id varchar(20), --图书号
+	@Book_in_price float,--进价
+	@Book_out_price float,--图书售价
+	@Book_name varchar(20),--书名
+	@Supplier_name varchar(20),--供应商名字
+	@Book_stock int--库存
+as
+insert into Book(Book_id,Book_in_price,Book_out_price,Book_name,Supplier_name,Book_stock)
+values (@Book_id, @Book_in_price, @Book_out_price, @Book_name, @Supplier_name, @Book_stock)
+
+
+exec add_book_message 'BK10000', 31.00, 44.00, '概率论', '科学出版社', 5 
+exec add_book_message 'BK10001', 37.00, 50.00, '算法导论', '北京出版社', 5 
+exec add_book_message 'BK10002', 24.00, 37.00, '数据库开发与应用', '人民邮电出版社', 5 
+exec add_book_message 'BK10003', 30.00, 43.00, 'java程序设计教程', '高等教育出版社', 5 
+exec add_book_message 'BK10004', 49.00, 62.00, '数据库系统概论', '清华大学出版社', 5 
+exec add_book_message 'BK10005', 24.00, 37.00, '算法与数据结构', '中国人民大学出版社', 5 
+exec add_book_message 'BK10006', 38.00, 51.00, '离散数学', '科学出版社', 5 
+exec add_book_message 'BK10007', 38.00, 51.00, '概率论', '北京出版社', 5 
+exec add_book_message 'BK10008', 42.00, 55.00, '算法导论', '人民邮电出版社', 5 
+exec add_book_message 'BK10009', 34.00, 47.00, '数据库开发与应用', '高等教育出版社', 5 
+exec add_book_message 'BK10010', 25.00, 38.00, 'java程序设计教程', '清华大学出版社', 5 
+exec add_book_message 'BK10011', 25.00, 38.00, '数据库系统概论', '中国人民大学出版社', 5 
+exec add_book_message 'BK10012', 21.00, 34.00, '算法与数据结构', '科学出版社', 5 
+exec add_book_message 'BK10013', 47.00, 60.00, '离散数学', '北京出版社', 5 
+exec add_book_message 'BK10014', 21.00, 34.00, '概率论', '人民邮电出版社', 5 
+exec add_book_message 'BK10015', 31.00, 44.00, '算法导论', '高等教育出版社', 5 
+
+
+
+
+------------------------------------------------------------------------------------------------
+drop proc add_supplier_message
+go 
+create procedure add_supplier_message
+	@Supplier_id varchar(20),--供应商号
+	@Supplier_name varchar(20),--供应商名字
+	@Supplier_city varchar(20),--供应商城市
+	@Supplier_tel varchar(20)--供应商电话
+as
+insert into supplier(Supplier_id, Supplier_name, Supplier_city, Supplier_tel)
+values (@Supplier_id, @Supplier_name, @Supplier_city, @Supplier_tel)
+exec add_supplier_message 'GY10001', '人民邮电出版社', '北京','18010411467'
+exec add_supplier_message 'GY10002', '高等教育出版社', '上海','18013341500'
+exec add_supplier_message 'GY10003', '清华大学出版社', '广州','18011691724'
+exec add_supplier_message 'GY10004', '中国人民大学出版社', '北京','18014781358'
+exec add_supplier_message 'GY10005', '科学出版社', '深圳','18019621464'
+exec add_supplier_message 'GY10006', '北京出版社', '北京','18017051145'
+
+
+select * from supplier
+select * from book
