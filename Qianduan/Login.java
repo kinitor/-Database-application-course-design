111a000fd2c4a576fef60f3ca5fccb51d9e2724c
YLH <42891308+Q1311605467@users.noreply.github.com>
2019-06-14 14:20:48 +0800

    Update Login.java
    添加了按钮响应，以及实现与Front窗口连接

--- Qianduan/Login.java
+++ Qianduan/Login.java
@@ -1,16 +1,21 @@
-package keshe;
+package Frame;
 
 import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.SwingConstants;
-import javax.swing.JButton;
-import javax.swing.JTextField;
+import javax.swing.JOptionPane;
 import javax.swing.JPasswordField;
-import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
-public class Login {
+public class Login  {
 
 	private JFrame frame;
 	private JTextField textField;
@@ -44,8 +49,8 @@ public class Login {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setTitle("\u4E66\u5E97\u9500\u552E\u7BA1\u7406\u7CFB\u7EDF");
-		frame.setBounds(100, 100, 450, 300);
+		frame.setTitle("\u4E66\u5E97\u9500\u552E\u7BA1\u7406\u7CFB\u7EDF");//
+		frame.setBounds(100, 100, 464, 305);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		frame.setLocationRelativeTo(null);
@@ -61,20 +66,43 @@ public class Login {
 		frame.getContentPane().add(label_1);
 		
 		JButton button = new JButton("\u91CD\u7F6E");
+		button.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				textField.setText("");
+				passwordField.setText("");
+			}
+		});
 		button.setBounds(80, 169, 113, 51);
 		frame.getContentPane().add(button);
 		
 		JButton button_1 = new JButton("\u767B\u5F55");
+		button_1.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				String name = textField.getText();
+				String password = passwordField.getText();
+				if("sa".equals(name)&&"123456".equals(password)) {//equals函数进行用户名和密码的匹配
+					//JOptionPane.showMessageDialog(null,"欢迎您");///窗口显示
+					new Front();//打开新窗口
+					frame.setVisible(false);
+					
+				}else {
+					JOptionPane.showMessageDialog(null, "错误的用户名和密码");
+				}
+			}
+		});
 		button_1.setBounds(240, 169, 113, 51);
 		frame.getContentPane().add(button_1);
 		
-		textField = new JTextField();
+		textField = new JTextField();///管理员
 		textField.setBounds(172, 55, 181, 24);
 		frame.getContentPane().add(textField);
 		textField.setColumns(10);
 		
-		passwordField = new JPasswordField();
+		passwordField = new JPasswordField();//密码
 		passwordField.setBounds(172, 102, 181, 24);
+		passwordField.setEchoChar('*');///将密码转化为*
 		frame.getContentPane().add(passwordField);
 		
 		JLabel label_2 = new JLabel("\u767B\u5F55");
